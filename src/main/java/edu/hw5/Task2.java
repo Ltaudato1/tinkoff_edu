1c194bde11bda91c976c88b515a6a4885535ae56
Ltaudato1 <gitta.tawer13@gmail.com>
2023-11-11 12:34:53 +0300

    brand new nextFriday13
    Не сразу увидел вторую часть во втором задании

--- src/main/java/edu/hw5/Task2.java
+++ src/main/java/edu/hw5/Task2.java
@@ -1,7 +1,11 @@
 package edu.hw5;
 
 import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,15 +16,15 @@ public class Task2 {
 
     private static final int FRIDAY_THIRTEENTH = 13;
 
-    public static List<LocalDateTime> getAllFriday13(LocalDateTime startDate) {
+    public static List<LocalDate> getAllFriday13(LocalDate startDate) {
         if (startDate == null) {
             return null;
         }
 
-        LocalDateTime endDate = LocalDateTime.of(startDate.getYear() + 1, 1, 1, 0, 0);
+        LocalDate endDate = LocalDate.of(startDate.getYear() + 1, 1, 1);
 
-        List<LocalDateTime> result = new ArrayList<>();
-        for (LocalDateTime currentDate = startDate;
+        List<LocalDate> result = new ArrayList<>();
+        for (LocalDate currentDate = startDate;
              currentDate.isBefore(endDate);
              currentDate = currentDate.plusDays(1)) {
             if (currentDate.getDayOfWeek() == DayOfWeek.FRIDAY && currentDate.getDayOfMonth() == FRIDAY_THIRTEENTH) {
@@ -28,5 +32,28 @@ public class Task2 {
             }
         }
         return result;
+    }
+
+    public static class NextFriday13 implements TemporalAdjuster {
+        @Override
+        public Temporal adjustInto(Temporal temporal) {
+            if (temporal == null) {
+                return null;
+            }
+
+            LocalDate date = LocalDate.from(temporal);
+            LocalDate nextFriday13th = LocalDate.of(date.getYear(), date.getMonthValue(), date.getDayOfMonth())
+                .with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
+
+            while (nextFriday13th.getDayOfMonth() != 13) {
+                nextFriday13th = LocalDate.of(nextFriday13th.getYear(),
+                        nextFriday13th.getMonthValue(),
+                        nextFriday13th.getDayOfMonth())
+                    .with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
+            }
+
+            return nextFriday13th;
+        }
+
     }
 }
--- src/test/java/edu/hw5/Test2.java
+++ src/test/java/edu/hw5/Test2.java
@@ -4,30 +4,31 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Stream;
 import static edu.hw5.Task2.getAllFriday13;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class Test2 {
-    static Stream<Arguments> provideData() {
+    static Stream<Arguments> provideData1() {
         return Stream.of(
             Arguments.of(
                 // [1925-02-13, 1925-03-13, 1925-11-13]
-                LocalDateTime.of(1925, 1, 1, 0, 0),
+                LocalDate.of(1925, 1, 1),
                 List.of(
-                    LocalDateTime.of(1925, 2, 13, 0, 0),
-                    LocalDateTime.of(1925, 3, 13, 0, 0),
-                    LocalDateTime.of(1925, 11, 13, 0, 0)
+                    LocalDate.of(1925, 2, 13),
+                    LocalDate.of(1925, 3, 13),
+                    LocalDate.of(1925, 11, 13)
                 )
             ),
             Arguments.of(
                 // [2024-09-13, 2024-12-13]
-                LocalDateTime.of(2024, 1, 1, 0, 0),
+                LocalDate.of(2024, 1, 1),
                 List.of(
-                    LocalDateTime.of(2024, 9, 13, 0, 0),
-                    LocalDateTime.of(2024, 12, 13, 0, 0)
+                    LocalDate.of(2024, 9, 13),
+                    LocalDate.of(2024, 12, 13)
                 )
             ),
             Arguments.of(null, null)
@@ -35,10 +36,36 @@ public class Test2 {
     }
 
     @ParameterizedTest
-    @DisplayName("Sample test")
-    @MethodSource("provideData")
-    void test(LocalDateTime input, List<LocalDateTime> answer) {
-        List<LocalDateTime> result = getAllFriday13(input);
+    @DisplayName("Sample test for getAllFriday13")
+    @MethodSource("provideData1")
+    void test1(LocalDate input, List<LocalDate> answer) {
+        List<LocalDate> result = getAllFriday13(input);
         assertEquals(answer, result);
+    }
+
+    static Stream<Arguments> provideData2() {
+        return Stream.of(
+            Arguments.of(
+                LocalDate.of(1925, 1, 1),
+                LocalDate.of(1925, 2, 13)
+            ),
+            Arguments.of(
+                LocalDate.of(2024, 1, 1),
+                LocalDate.of(2024, 9, 13)
+            ),
+            Arguments.of(null, null)
+        );
+    }
+
+    @ParameterizedTest
+    @DisplayName("Sample test for getNextFriday13")
+    @MethodSource("provideData2")
+    void test2(LocalDate input, LocalDate answer) {
+        try {
+            LocalDate result = input.with(new Task2.NextFriday13());
+            assertEquals(answer, result);
+        } catch (NullPointerException e) {
+            assertNull(input);
+        }
     }
 }
